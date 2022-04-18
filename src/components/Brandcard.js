bde6d82c1edf9b2fb904302dedd966b29633f4f2
rohit katbamna <rohitkatbamna@protonmail.com>
2022-04-18 09:54:52 +0530

    09:54 AM UNIQUE RANDOM IMAGES ADDED TO BRANDS

--- src/components/Brandcard.js
+++ src/components/Brandcard.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Brandcard({ brands }) {
+	const images = useSelector((state) => state.ImagesArray);
+	console.log(images);
+	const RandomNumber = Math.floor(Math.random() * images.length);
+
 	return (
 		<>
 			<div className="card ms-auto me-auto mt-5" style={{ width: "18rem" }}>
+				<img src={images[RandomNumber].Image} class="card-img-top" alt="..." />
 				<div className="card-body">
 					<h4 className="card-title fw-bold">{brands}</h4>
 				</div>
